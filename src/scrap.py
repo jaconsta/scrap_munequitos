29083261fae081751d698e48f3770b5a495c8c65
Javier Constain <constainopia@gmail.com>
2019-05-01 22:02:02 -0500

    Adjust chapters fetch

--- src/scrap.py
+++ src/scrap.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from collections import deque
 
 import requests
 from bs4 import BeautifulSoup
@@ -14,7 +15,10 @@ expected_munequitos = [
     'the_seven_deadly_sins'
 ]
 
-# get_li_name = lambda x: [y for y in x[0].contents if type(y) == NavigableString][0].string
+from_chapters = {
+    'the_seven_deadly_sins': 388
+}
+
 def get_li_name (x):
     content = x.contents[0]
     text = filter(lambda x: type(x) == NavigableString, content)
@@ -23,7 +27,7 @@ def get_li_name (x):
 # get_li_name = lambda x: repr(x.contents[0].contents[2].string)
 get_li_date = lambda x: x.contents[0].contents[0].string
 get_li_link = lambda x: x.find_all('a')[0].get('href')
-get_file_name = lambda x: x.strip().lower().replace(' ', '_')
+get_file_name = lambda x: x.strip().lower().replace(' ', '_').replace('.', '_')
 
 def get_li_values(x):
     name = get_li_name(x)
@@ -33,45 +37,65 @@ def get_li_values(x):
 # x = [ul.find_all('a')[0] for ul in soup.find_all('ul', class_='new-list')[0].children if type(ul) != NavigableString]
 x = [get_li_values(ul) for ul in soup.find_all('ul', class_='new-list')[0].children if type(ul) != NavigableString]
 
-munequitos_i_want = filter(lambda x: x['joined_name'] in expected_munequitos, x)
 
-## a for is required
-# munequito_item_details = next(munequitos_i_want)
+#
+#
+# [(comic.text, comic.a.get('href')) for comic in soup.find_all('ul', class_='dropdown-menu')[0].children if type(comic) != NavigableString]
+# (comic.text, comic.a.get('href')
+#
+#
+pre_list = soup.find_all('ul', class_='dropdown-menu')[0].children
+list_path = deque(pre_list, maxlen=2)[0].a.get('href')  # Gets the last two
+list_site = requests.get(f'{site}{list_path}')
+list_soup = BeautifulSoup(list_site.text, 'html.parser')
+
+all_munequitos = [(x.a.get('href'), x.a.text, get_file_name(x.a.text)) for x in list_soup.table.find_all('tr')[1:]]
+
+munequitos_i_want = filter(lambda x: x[-1] in expected_munequitos, all_munequitos)
+
 
 for munequito_item_details in munequitos_i_want:
+    munequito_chapters = requests.get(f'{site}{munequito_item_details[0]}')
+    chapters_soup = BeautifulSoup(munequito_chapters.text, 'html.parser')
+    chapter_list = [(chap.a.get('href'), chap.a.text, get_file_name(chap.a.text)) for chap in chapters_soup.table.find_all('tr')[1:]]
+
     # split the url for extraction and post url creation
-    munequito_url_components = munequito_item_details['link'].split('/')
     # Create munequito image folder
-    munequito_folder = f'downloaded/{munequito_item_details["joined_name"]}'
-    episode_number = munequito_url_components[-3]
-    episode_folder = f'{munequito_folder}/{episode_number}'
+    munequito_folder = f'downloaded/{munequito_item_details[-1]}'
+    print(munequito_folder)
 
     if not os.path.exists(munequito_folder):
         os.mkdir(munequito_folder)
-    if not os.path.exists(episode_folder):
-        os.mkdir(episode_folder)
 
-    status = 200  # Default html response code
-    page_number = 1  # It should increase for each page name
-    while status == 200:
-        munequito_url_components[-1] = str(page_number)
-        page_url = '/'.join(munequito_url_components)
-        munequito_page = requests.get(f'{site}{page_url}')
-        status = munequito_page.status_code
-        munequito_soup = BeautifulSoup(munequito_page.text, 'html.parser')
+    for chapter in chapter_list:
+        chap = chapter[-1].split('-')[0].strip()[:-1]
+        print(f'chapter: {chap} {chapter[1]}')
+        episode_folder = f'{munequito_folder}/{chap}'
+        if not os.path.exists(episode_folder):
+            os.mkdir(episode_folder)
 
-        if 'Page Not Found' in munequito_soup.title.text:
-            status = 404
-            break
+        chapter_path = chapter[0][:-1]
 
-        munequito_img_url = munequito_soup.img.get('src')
-        img_response = requests.get(f'https:{munequito_img_url}', stream=True)
+        status = 200  # Default html response code
+        page_number = 1  # It should increase for each page name
+        while status == 200:
+            print(page_number)
+            munequito_page = requests.get(f'{site}{chapter_path}{str(page_number)}')
+            status = munequito_page.status_code
+            munequito_soup = BeautifulSoup(munequito_page.text, 'html.parser')
 
-        with open(f'{episode_folder}/{page_number}.{munequito_img_url.split(".")[-1]}', 'wb') as page:
-            shutil.copyfileobj(img_response.raw, page)
+            if 'Page Not Found' in munequito_soup.title.text:
+                status = 404
+                break
 
-        del img_response
-        page_number += 1
+            munequito_img_url = munequito_soup.img.get('src')
+            img_response = requests.get(f'https:{munequito_img_url}', stream=True)
+
+            with open(f'{episode_folder}/{page_number}.{munequito_img_url.split(".")[-1]}', 'wb') as page:
+                shutil.copyfileobj(img_response.raw, page)
+
+            del img_response
+            page_number += 1
 
 
 print('hello')
